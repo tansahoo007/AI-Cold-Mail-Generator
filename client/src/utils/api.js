128262e9cef0c3908b57844ae158bcf35370dd65
tansahoo007 <tanmayasahoo966@gmail.com>
2026-08-19 20:08:06 +0530

    Fix production API URL

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 // });
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api' 
+    baseURL: 'https://ai-cold-mail-generator-lgt1.onrender.com/api' 
 });
 
 api.interceptors.request.use((config) => {
